01cb6aa36f2c4ce9572761102efa0fea78e076ac
Alstocras <alstoperiment@gmail.com>
2026-05-12 15:27:17 +0000

    made navbar prototype

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,13 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
+<header
+	class="h-0.125 flex flex-row items-center gap-6 bg-primary p-4 font-navbar text-navbar text-accent"
+>
+	<a href="/">home</a>
+	<a href="/upload">upload books</a>
+</header>
+
 <main
 	class="flex min-h-screen flex-col items-center bg-background font-normal text-normal text-secondary"
 >
