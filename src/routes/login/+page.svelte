3ebc4bb2428e77fa638587a0e32418349175b68f
Alstocras <alstoperiment@gmail.com>
2026-05-12 17:26:04 +0000

    added username temp login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,29 @@
+<script lang="ts">
+	let username: string = $state('');
+</script>
+
 <h1 class="text-3xl">Log in</h1>
 
 <br /><br />
 
-<label for="name">Your name</label>
-<input type="text" placeholder="username" id="name" class="bg-input text-accent" />
+<label for="name">Username</label>
+<input
+	type="text"
+	placeholder="username"
+	id="name"
+	class="
+		w-full
+		max-w-md
+		rounded-lg
+		border-2
+		border-primary/20
+		p-3
+		text-input
+		transition-all
+		outline-none
+		focus:border-primary
+	"
+	bind:value={username}
+/>
+<br />
+<p>Hello, {username}.</p>
